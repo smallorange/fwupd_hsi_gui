GNOME/gnome-control-center

commit 551d72280bbc9479a81697e2d888a77a64538b27

network: Stop using deprecated nm_device_*_get_hw_address
There is a general nm_device_get_hw_address which has been available since
NetworkManager 0.9.10.

panels/network/connection-editor/ce-page-details.c
@@ -291,11 +291,7 @@ connect_details_page (CEPageDetails *self)
         gtk_widget_set_visible (GTK_WIDGET (self->speed_heading_label), speed_label != NULL);
         gtk_widget_set_visible (GTK_WIDGET (self->speed_label), speed_label != NULL);
 
-        if (NM_IS_DEVICE_WIFI (self->device))
-                hw_address = nm_device_wifi_get_hw_address (NM_DEVICE_WIFI (self->device));
-        else if (NM_IS_DEVICE_ETHERNET (self->device))
-                hw_address = nm_device_ethernet_get_hw_address (NM_DEVICE_ETHERNET (self->device));
-
+        hw_address = nm_device_get_hw_address (self->device);
         gtk_label_set_label (self->mac_label, hw_address);
         gtk_widget_set_visible (GTK_WIDGET (self->mac_heading_label), hw_address != NULL);
         gtk_widget_set_visible (GTK_WIDGET (self->mac_label), hw_address != NULL);

panels/network/net-device-ethernet.c
@@ -147,8 +147,7 @@ add_details (GtkWidget *details, NMDevice *device, NMConnection *connection)
                 add_details_row (details, i++, _("IPv6 Address"), ip6_address);
         }
 
-        add_details_row (details, i++, _("Hardware Address"),
-                         nm_device_ethernet_get_hw_address (NM_DEVICE_ETHERNET (device)));
+        add_details_row (details, i++, _("Hardware Address"), nm_device_get_hw_address (device));
 
         if (ip4_route)
                 add_details_row (details, i++, _("Default Route"), ip4_route);

panels/network/panel-common.c
@@ -290,30 +290,6 @@ get_mac_address_of_connection (NMConnection *connection)
         return NULL;
 }
 
-static const gchar *
-get_mac_address_of_device (NMDevice *device)
-{
-        const gchar *mac = NULL;
-        switch (nm_device_get_device_type (device)) {
-        case NM_DEVICE_TYPE_WIFI:
-        {
-                NMDeviceWifi *device_wifi = NM_DEVICE_WIFI (device);
-                mac = nm_device_wifi_get_hw_address (device_wifi);
-                break;
-        }
-        case NM_DEVICE_TYPE_ETHERNET:
-        {
-                NMDeviceEthernet *device_ethernet = NM_DEVICE_ETHERNET (device);
-                mac = nm_device_ethernet_get_hw_address (device_ethernet);
-                break;
-        }
-        default:
-                break;
-        }
-        /* no MAC address found */
-        return mac;
-}
-
 /* returns TRUE if both MACs are equal */
 static gboolean
 compare_mac_device_with_mac_connection (NMDevice *device,
@@ -322,7 +298,7 @@ compare_mac_device_with_mac_connection (NMDevice *device,
         const gchar *mac_dev = NULL;
         g_autofree gchar *mac_conn = NULL;
 
-        mac_dev = get_mac_address_of_device (device);
+        mac_dev = nm_device_get_hw_address (device);
         if (mac_dev == NULL)
                 return FALSE;
 

smallorange/fwupd_hsi_gui

commit 544cba33b5bcecd955962476737067aea652096f

network: fix critical when opening VPN details page
I was seeing this critical when opening the VPN details page:

(gnome-control-center:78888): nm-CRITICAL **: 16:26:21.689: ((src/libnm-client-impl/nm-device.c:1299)): assertion '<dropped>' failed

It probably broke in 551d72280bbc9479a81697e2d888a77a64538b27. The code
above and below expects self->device may be NULL, so let's add a
NULL check here.

panels/network/connection-editor/ce-page-details.c
@@ -297,7 +297,9 @@ connect_details_page (CEPageDetails *self)
         gtk_widget_set_visible (GTK_WIDGET (self->speed_heading_label), speed_label != NULL);
         gtk_widget_set_visible (GTK_WIDGET (self->speed_label), speed_label != NULL);
 
-        hw_address = nm_device_get_hw_address (self->device);
+        if (self->device)
+            hw_address = nm_device_get_hw_address (self->device);
+
         gtk_label_set_label (self->mac_label, hw_address);
         gtk_widget_set_visible (GTK_WIDGET (self->mac_heading_label), hw_address != NULL);
         gtk_widget_set_visible (GTK_WIDGET (self->mac_label), hw_address != NULL);